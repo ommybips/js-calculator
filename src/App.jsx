24c9d9807c2d28a199106531c6f9fb63a9b3e416
Irene Phamorn <irenie.p@hotmail.com>
2025-01-06 21:22:41 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="calculator">
+        <div className="display">0</div>
+        <div className="buttons">
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
