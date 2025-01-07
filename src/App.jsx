8d9e11cfba905e5956b1ff44c843ae50a4a99b72
Irene Phamorn <irenie.p@hotmail.com>
2025-01-08 00:41:40 +0700

    logic for calculator

--- src/App.jsx
+++ src/App.jsx
@@ -4,80 +4,324 @@ import "./App.css";
 function App() {
   const [display, setDisplay] = useState("0");
   const [formula, setFormula] = useState("");
+  const [justEvaluated, setJustEvaluated] = useState(false);
 
   const handleClick = (e) => {
     switch (e.target.id) {
-      case "ac":
+      case "clear":
         setDisplay("0");
         setFormula("");
         break;
       case "one":
-        setDisplay(display === "0" ? "1" : display + "1");
-        setFormula(formula + "1");
+        if (justEvaluated) {
+          setDisplay("1");
+          setFormula("1");
+          setJustEvaluated(false);
+          return;
+        }
+        setDisplay(
+          display === "0"
+            ? "1"
+            : display === "+"
+            ? "1"
+            : display === "-"
+            ? "1"
+            : display === "*-"
+            ? "1"
+            : display === "/"
+            ? "1"
+            : display + "1"
+        );
+        setFormula(formula === "0" ? "1" : formula + "1");
         break;
       case "two":
-        setDisplay(display === "0" ? "2" : display + "2");
-        setFormula(formula + "2");
+        if (justEvaluated) {
+          setDisplay("2");
+          setFormula("2");
+          setJustEvaluated(false);
+          return;
+        }
+        setDisplay(
+          display === "0"
+            ? "2"
+            : display === "+"
+            ? "2"
+            : display === "-"
+            ? "2"
+            : display === "*-"
+            ? "2"
+            : display === "/"
+            ? "2"
+            : display + "2"
+        );
+        setFormula(formula === "0" ? "2" : formula + "2");
         break;
       case "three":
-        setDisplay(display === "0" ? "3" : display + "3");
-        setFormula(formula + "3");
+        if (justEvaluated) {
+          setDisplay("3");
+          setFormula("3");
+          setJustEvaluated(false);
+          return;
+        }
+        setDisplay(
+          display === "0"
+            ? "3"
+            : display === "+"
+            ? "3"
+            : display === "-"
+            ? "3"
+            : display === "*-"
+            ? "3"
+            : display === "/"
+            ? "3"
+            : display + "3"
+        );
+        setFormula(formula === "0" ? "3" : formula + "3");
         break;
       case "four":
-        setDisplay(display === "0" ? "4" : display + "4");
-        setFormula(formula + "4");
+        if (justEvaluated) {
+          setDisplay("4");
+          setFormula("4");
+          setJustEvaluated(false);
+          return;
+        }
+        setDisplay(
+          display === "0"
+            ? "4"
+            : display === "+"
+            ? "4"
+            : display === "-"
+            ? "4"
+            : display === "*-"
+            ? "4"
+            : display === "/"
+            ? "4"
+            : display + "4"
+        );
+        setFormula(formula === "0" ? "4" : formula + "4");
         break;
       case "five":
-        setDisplay(display === "0" ? "5" : display + "5");
-        setFormula(formula + "5");
+        if (justEvaluated) {
+          setDisplay("5");
+          setFormula("5");
+          setJustEvaluated(false);
+          return;
+        }
+        setDisplay(
+          display === "0"
+            ? "5"
+            : display === "+"
+            ? "5"
+            : display === "-"
+            ? "5"
+            : display === "*-"
+            ? "5"
+            : display === "/"
+            ? "5"
+            : display + "5"
+        );
+        setFormula(formula === "0" ? "5" : formula + "5");
         break;
       case "six":
-        setDisplay(display === "0" ? "6" : display + "6");
-        setFormula(formula + "6");
+        if (justEvaluated) {
+          setDisplay("6");
+          setFormula("6");
+          setJustEvaluated(false);
+          return;
+        }
+        setDisplay(
+          display === "0"
+            ? "6"
+            : display === "+"
+            ? "6"
+            : display === "-"
+            ? "6"
+            : display === "*-"
+            ? "6"
+            : display === "/"
+            ? "6"
+            : display + "6"
+        );
+        setFormula(formula === "0" ? "6" : formula + "6");
         break;
       case "seven":
-        setDisplay(display === "0" ? "7" : display + "7");
-        setFormula(formula + "7");
+        if (justEvaluated) {
+          setDisplay("7");
+          setFormula("7");
+          setJustEvaluated(false);
+          return;
+        }
+        setDisplay(
+          display === "0"
+            ? "7"
+            : display === "+"
+            ? "7"
+            : display === "-"
+            ? "7"
+            : display === "*-"
+            ? "7"
+            : display === "/"
+            ? "7"
+            : display + "7"
+        );
+        setFormula(formula === "0" ? "7" : formula + "7");
         break;
       case "eight":
-        setDisplay(display === "0" ? "8" : display + "8");
-        setFormula(formula + "8");
+        if (justEvaluated) {
+          setDisplay("8");
+          setFormula("8");
+          setJustEvaluated(false);
+          return;
+        }
+        setDisplay(
+          display === "0"
+            ? "8"
+            : display === "+"
+            ? "8"
+            : display === "-"
+            ? "8"
+            : display === "*-"
+            ? "8"
+            : display === "/"
+            ? "8"
+            : display + "8"
+        );
+        setFormula(formula === "0" ? "8" : formula + "8");
         break;
       case "nine":
-        setDisplay(display === "0" ? "9" : display + "9");
-        setFormula(formula + "9");
+        if (justEvaluated) {
+          setDisplay("9");
+          setFormula("9");
+          setJustEvaluated(false);
+          return;
+        }
+        setDisplay(
+          display === "0"
+            ? "9"
+            : display === "+"
+            ? "9"
+            : display === "-"
+            ? "9"
+            : display === "*-"
+            ? "9"
+            : display === "/"
+            ? "9"
+            : display + "9"
+        );
+        setFormula(formula === "0" ? "9" : formula + "9");
         break;
       case "zero":
-        setDisplay(display === "0" ? "0" : display + "0");
-        setFormula(formula + "0");
+        if (justEvaluated) {
+          setDisplay("0");
+          setFormula("0");
+          setJustEvaluated(false);
+          return;
+        }
+        setDisplay(
+          display === "0"
+            ? display
+            : display === "+"
+            ? display
+            : display === "-"
+            ? display
+            : display === "*-"
+            ? display
+            : display === "/"
+            ? display
+            : display + "0"
+        );
+        setFormula(
+          display === "0"
+            ? display
+            : formula[formula.length - 1] === "+"
+            ? formula
+            : formula[formula.length - 1] === "-"
+            ? formula
+            : formula[formula.length - 1] === "*-"
+            ? formula
+            : formula[formula.length - 1] === "/"
+            ? formula
+            : formula + "0"
+        );
         break;
-      case "dot":
+      case "decimal":
         if (!display.includes(".")) {
           setDisplay(display + ".");
           setFormula(formula + ".");
         }
         break;
-      case "plus":
-        setDisplay("+");
-        setFormula(formula + "+");
+      case "add":
+        if (formula === "" || formula === "0") return;
+        if (
+          formula[formula.length - 1] === "+" ||
+          formula[formula.length - 1] === "*" ||
+          formula[formula.length - 1] === "/"
+        ) {
+          setDisplay("+");
+          setFormula(formula.slice(0, -1) + "+");
+        } else if (formula[formula.length - 1] === "-") {
+          setDisplay("+");
+          setFormula(formula.slice(0, -2) + "+");
+        } else {
+          setDisplay("+");
+          setFormula(formula + "+");
+          setJustEvaluated(false);
+        }
         break;
-      case "minus":
-        setDisplay("-");
-        setFormula(formula + "-");
+      case "subtract":
+        if (formula === "" || formula === "0") return;
+        if (formula[formula.length - 1] === "-") {
+          setDisplay("-");
+          setFormula(formula.slice(0, -1) + "-");
+        } else {
+          setDisplay("-");
+          setFormula(formula + "-");
+          setJustEvaluated(false);
+        }
         break;
       case "multiply":
-        setDisplay("*");
-        setFormula(formula + "*");
+        if (formula === "" || formula === "0") return;
+        if (
+          formula[formula.length - 1] === "+" ||
+          formula[formula.length - 1] === "*" ||
+          formula[formula.length - 1] === "/"
+        ) {
+          setDisplay("*");
+          setFormula(formula.slice(0, -1) + "*");
+        } else if (formula[formula.length - 1] === "-") {
+          setDisplay("*");
+          setFormula(formula.slice(0, -2) + "*");
+        } else {
+          setDisplay("*");
+          setFormula(formula + "*");
+          setJustEvaluated(false);
+        }
         break;
       case "divide":
-        setDisplay("/");
-        setFormula(formula + "/");
+        if (formula === "" || formula === "0") return;
+        if (
+          formula[formula.length - 1] === "+" ||
+          formula[formula.length - 1] === "-" ||
+          formula[formula.length - 1] === "*" ||
+          formula[formula.length - 1] === "/"
+        ) {
+          setDisplay("/");
+          setFormula(formula.slice(0, -1) + "/");
+        } else if (formula[formula.length - 1] === "-") {
+          setDisplay("/");
+          setFormula(formula.slice(0, -2) + "/");
+        } else {
+          setDisplay("/");
+          setFormula(formula + "/");
+          setJustEvaluated(false);
+        }
         break;
-      case "equal":
+      case "equals":
         if (formula === "") return setFormula(display);
         else {
           setDisplay(eval(formula));
           setFormula(eval(formula));
+          setJustEvaluated(true);
         }
     }
   };
@@ -90,7 +334,7 @@ function App() {
           {display}
         </div>
         <div className="buttons">
-          <button id="ac" className="big-btn" onClick={handleClick}>
+          <button id="clear" className="big-btn" onClick={handleClick}>
             AC
           </button>
           <button id="divide" className="symbols" onClick={handleClick}>
@@ -108,7 +352,7 @@ function App() {
           <button id="nine" onClick={handleClick}>
             9
           </button>
-          <button id="minus" className="symbols" onClick={handleClick}>
+          <button id="subtract" className="symbols" onClick={handleClick}>
             -
           </button>
           <button id="four" onClick={handleClick}>
@@ -120,7 +364,7 @@ function App() {
           <button id="six" onClick={handleClick}>
             6
           </button>
-          <button id="plus" className="symbols" onClick={handleClick}>
+          <button id="add" className="symbols" onClick={handleClick}>
             +
           </button>
           <button id="one" onClick={handleClick}>
@@ -132,13 +376,13 @@ function App() {
           <button id="three" onClick={handleClick}>
             3
           </button>
-          <button id="equal" onClick={handleClick}>
+          <button id="equals" onClick={handleClick}>
             =
           </button>
           <button id="zero" className="big-btn" onClick={handleClick}>
             0
           </button>
-          <button id="dot" onClick={handleClick}>
+          <button id="decimal" onClick={handleClick}>
             .
           </button>
         </div>
